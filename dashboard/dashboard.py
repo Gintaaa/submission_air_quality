c3624fa17c478ac4a805215d96e1d48a37f256ad
Gintaaa <90619473+Gintaaa@users.noreply.github.com>
2026-02-23 04:32:05 +0700

    fix: update data path and requirements

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -1,3 +1,4 @@
+import os
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -6,7 +7,10 @@ import streamlit as st
 st.set_page_config(page_title="Air Quality Dashboard", layout="wide")
 
 # Load data
-df = pd.read_csv("dashboard/main_data.csv")
+#df = pd.read_csv("dashboard/main_data.csv")
+current_dir = os.path.dirname(__file__)
+file_path = os.path.join(current_dir, "main_data.csv")
+df = pd.read_csv(file_path)
 df['datetime'] = pd.to_datetime(df['datetime'])
 
 st.title('Dashboard Analisis Kualitas Udara (PM2.5) ☁️')
